e49bedfc764041cb3d925d6d841bf2775a038a50
Shriram Rajeshwar Nawathe <108632713+ShriramNawathe@users.noreply.github.com>
2023-03-28 14:43:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import SumCalculator from "./SumCalculator";
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <SumCalculator />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
